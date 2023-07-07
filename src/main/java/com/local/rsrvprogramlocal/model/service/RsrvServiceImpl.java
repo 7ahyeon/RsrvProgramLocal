0053fbeb5c1a857f453077d0909351db0f30795d
7ahyeon <7ahyeon@gmail.com>
2023-07-07 17:54:34 +0900

    FileReader 및 BufferedReader 사용

--- src/main/java/com/local/rsrvprogramlocal/model/service/RsrvServiceImpl.java
+++ src/main/java/com/local/rsrvprogramlocal/model/service/RsrvServiceImpl.java
@@ -27,19 +27,18 @@ public class RsrvServiceImpl implements RsrvService {
         String fileName = "RsrvReqRq.json";
         URL resource = getClass().getClassLoader().getResource("static/file/" + fileName);
         String jsonFilePath = resource.getFile();
-        File file = new File(jsonFilePath);
 
-        FileInputStream fis = null;
-        InputStreamReader isr = null;
+        FileReader fr = null;
         BufferedReader br = null;
-        String jsonFileContentLine;
 
         try {
-            fis = new FileInputStream(file);
-            isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
-            br = new BufferedReader(isr);
-            // StringBuilder : 문자열 연산이 많을 경우, 동기화를 고려하지 않기 때문에 단일 스레드 환경일 경우(StringBuffer보다 성능 뛰어남) 사용
-            StringBuilder sb = new StringBuilder();
+            fr = new FileReader(jsonFilePath);
+            br = new BufferedReader(fr);
+
+            String jsonFileContentLine;
+            // StringBuilder : 동기화 비지원 (싱글 스레드 환경 적합)
+            // StringBuffer : 동기화 지원 (멀티 스레드 환경 적합)
+            StringBuffer sb = new StringBuffer();
 
             while((jsonFileContentLine = br.readLine()) != null){ // 빈 문자열이 없도록 주의(NPE)
                 sb.append(jsonFileContentLine);
@@ -49,25 +48,18 @@ public class RsrvServiceImpl implements RsrvService {
         } catch (IOException e) {
             throw new FileNotReadException(e);
         } finally {
+            if (fr != null) {
+                try {
+                    fr.close();
+                } catch (IOException e) {
+                    throw new CloseException("FileReader is not closed.", e);
+                }
+            }
             if (br != null) {
                 try {
                     br.close();
                 } catch (IOException e) {
-                    throw new CloseException("BufferedReader is not closed", e);
-                }
-            }
-            if (isr != null) {
-                try {
-                    isr.close();
-                } catch (IOException e) {
-                    throw new CloseException("InputStreamReader is not closed", e);
-                }
-            }
-            if (fis != null) {
-                try {
-                    fis.close();
-                } catch (IOException e) {
-                    throw new CloseException("FileInputStream is not closed", e);
+                    throw new CloseException("BufferedReader is not closed.", e);
                 }
             }
         }
